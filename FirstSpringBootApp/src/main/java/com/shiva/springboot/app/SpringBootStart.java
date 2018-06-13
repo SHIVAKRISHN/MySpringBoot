05bd63cc4b343db6e292684f3e092ef780907a4a
SHIVAKRISHNA <shiva1607502.office@gmail.com>
2018-06-13 15:04:24 +0530

    Initial Commit of spring boot

---
+++ FirstSpringBootApp/src/main/java/com/shiva/springboot/app/SpringBootStart.java
@@ -0,0 +1,16 @@
+package com.shiva.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStart {
+
+	public static void main(String[] args) {
+ 
+		SpringApplication.run(SpringBootStart.class, args);
+		
+
+	}
+
+}
